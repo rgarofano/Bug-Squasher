67b1b65c6a5bdb936ddf8caa0027a54193d2fd26
Jen <45193617+jengu288@users.noreply.github.com>
2022-10-15 21:06:25 -0700

    Add initial text bug movement

--- app/src/main/java/ca/cmpt276/as3/WelcomeActivity.java
+++ app/src/main/java/ca/cmpt276/as3/WelcomeActivity.java
@@ -6,12 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class WelcomeActivity extends AppCompatActivity {
 
     private static final int DELAY_MILLIS = 4000;
     private Handler handler;
+    private Runnable runnable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,17 +24,35 @@ public class WelcomeActivity extends AppCompatActivity {
         getSupportActionBar().hide();
 
         //start android animations
+        Animation move = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.move);
+        TextView bug = findViewById(R.id.bug);
+        bug.startAnimation(move);
 
-        //delayed handler that starts the main activity
-        handler =  new Handler();
-        Runnable runnable = new Runnable() {
+        move.setAnimationListener(new Animation.AnimationListener() {
             @Override
-            public void run() {
-                startActivity(new Intent(WelcomeActivity.this, MainActivity.class));
-                finish();
+            public void onAnimationStart(Animation animation) {
+
             }
-        };
-        handler.postDelayed(runnable, DELAY_MILLIS);
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+            //delayed handler that starts the main activity
+                handler = new Handler();
+                runnable = new Runnable() {
+                    @Override
+                    public void run() {
+                        startActivity(new Intent(WelcomeActivity.this, MainActivity.class));
+                        finish();
+                    }
+                };
+                handler.postDelayed(runnable, DELAY_MILLIS);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
 
         //skip button override of handler
         Button skipButton = findViewById(R.id.skipButton);
